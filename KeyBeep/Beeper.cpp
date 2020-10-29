916b1d07bf91b8ba4b30aa1f08c9737a3d0fe646
Jimin Park <pjm950@gmail.com>
2020-10-29 19:29:12 +0900

    Implemented some of Beeper::Device

--- KeyBeep/Beeper.cpp
+++ KeyBeep/Beeper.cpp
@@ -10,6 +10,142 @@ inline void SAFE_RELEASE(T* t)
 	}
 }
 
+template<typename T>
+inline void SAFE_RELEASE(std::unique_ptr<T> t)
+{
+	if (t != nullptr)
+	{
+		(*t).Release();
+	}
+}
+
+Beeper::Device::Device(IMMDevice* device)
+	: mMMDevice(device)
+{
+	Initialize();
+}
+
+Beeper::Device::Device(IMMDeviceEnumerator* enumerator)
+{
+	HRESULT hRes = S_OK;
+	hRes = enumerator->GetDefaultAudioEndpoint(eRender, eConsole, &mMMDevice);
+
+	if (FAILED(hRes))
+	{
+		std::exit(1);
+	}
+
+	Initialize();
+}
+
+Beeper::Device::~Device()
+{
+	Stop();
+
+	SAFE_RELEASE(mMMDevice);
+}
+
+void Beeper::Device::Main()
+{
+	SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
+
+	while (mIsRunning.load(std::memory_order_relaxed))
+	{
+		assert(mAudioClient != nullptr);
+
+		// TODO
+	}
+}
+
+void Beeper::Device::Initialize()
+{
+	assert(mMMDevice != nullptr);
+
+	HRESULT hRes = S_OK;
+	hRes = mMMDevice->Activate(__uuidof(IAudioClient), CLSCTX_ALL, nullptr, reinterpret_cast<void**>(&mAudioClient));
+
+	if (FAILED(hRes))
+	{
+		std::exit(1);
+	}
+
+	WAVEFORMATEX* waveFormat = nullptr;
+	hRes = mAudioClient->GetMixFormat(&waveFormat);
+	if (FAILED(hRes))
+	{
+		std::exit(1);
+	}
+
+	mMixFormat = *reinterpret_cast<WAVEFORMATEXTENSIBLE *>(waveFormat);
+	CoTaskMemFree(waveFormat);
+}
+
+std::wstring Beeper::Device::GetFriendlyName() const
+{
+	IPropertyStore* properties;
+	const_cast<IMMDevice *>(mMMDevice)->OpenPropertyStore(STGM_READ, &properties);
+
+	PROPVARIANT varName;
+	::PropVariantInit(&varName);
+
+	properties->GetValue(PKEY_Device_FriendlyName, &varName);
+
+	return varName.pwszVal;
+}
+
+void Beeper::Device::Start()
+{
+	HRESULT hRes = S_OK;
+
+	assert(mMMDevice != nullptr);
+
+	mEventHandle = ::CreateEventW(nullptr, false, false, nullptr);
+	assert(mEventHandle != nullptr);
+
+	// TODO
+
+	hRes = mAudioClient->SetEventHandle(mEventHandle);
+	if (FAILED(hRes))
+	{
+		return;
+	}
+
+	hRes = mAudioClient->Start();
+	if (FAILED(hRes))
+	{
+		return;
+	}
+
+	if (mIsRunning.exchange(true) != false)
+	{
+		return;
+	}
+
+	mThread = std::thread(&Beeper::Device::Main, this);
+}
+
+void Beeper::Device::Stop()
+{
+	if (mIsRunning.exchange(false) == true)
+	{
+		if (mThread.joinable())
+		{
+			mThread.join();
+		}
+
+		if (mAudioClient)
+		{
+			mAudioClient->Stop();
+		}
+
+		if (mEventHandle)
+		{
+			CloseHandle(mEventHandle);
+			mEventHandle = nullptr;
+		}
+	}
+}
+
 Beeper::Beeper(KeyBeep& owner)
 	: mOwner(owner)
 {
@@ -28,26 +164,12 @@ Beeper::Beeper(KeyBeep& owner)
 		std::exit(1);
 	}
 
-	hRes = mDeviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &mDevice);
-	if (FAILED(hRes))
-	{
-		std::exit(1);
-	}
-
-	IPropertyStore* properties;
-	mDevice->OpenPropertyStore(STGM_READ, &properties);
-
-	PROPVARIANT varName;
-	::PropVariantInit(&varName);
-
-	properties->GetValue(PKEY_Device_FriendlyName, &varName);
-
-	std::wcout << L"Audio Device: " << varName.pwszVal << std::endl;
+	mDevice = std::make_unique<Device>(mDeviceEnumerator);
+	std::wcout << L"Device: " << mDevice->GetFriendlyName() << std::endl;
 }
 
 Beeper::~Beeper()
 {
 	SAFE_RELEASE(mDeviceEnumerator);
-	SAFE_RELEASE(mDevice);
 	CoUninitialize();
 }
--- KeyBeep/Beeper.h
+++ KeyBeep/Beeper.h
@@ -6,6 +6,37 @@ class KeyBeep;
 
 class Beeper
 {
+protected:
+	class Device
+	{
+	public:
+		Device(IMMDevice* device);
+		Device(IMMDeviceEnumerator* enumerator);
+		~Device();
+
+		Device(const Device&) = delete;
+		Device& operator= (const Device&) = delete;
+		Device& operator= (Device&&) = delete;
+
+		inline bool IsRunning() const { return mIsRunning.load(std::memory_order_relaxed); }
+		std::wstring GetFriendlyName() const;
+
+		void Start();
+		void Stop();
+
+	protected:
+		void Main();
+		void Initialize();
+
+		std::atomic<bool> mIsRunning = false;
+		std::thread mThread;
+
+		IMMDevice* mMMDevice = nullptr;
+		IAudioClient* mAudioClient = nullptr;
+		IAudioRenderClient* mAudioRenderClient = nullptr;
+		HANDLE mEventHandle = nullptr;
+		WAVEFORMATEXTENSIBLE mMixFormat;
+	};
 public:
 	Beeper(KeyBeep& owner);
 	~Beeper();
@@ -13,5 +44,6 @@ public:
 protected:
 	KeyBeep& mOwner;
 	IMMDeviceEnumerator* mDeviceEnumerator = nullptr;
-	IMMDevice* mDevice = nullptr;
+
+	std::unique_ptr<Device> mDevice = nullptr;
 };
